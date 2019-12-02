7b8cebf1dff563a572bca8dc2fd4d6b1fb409baf
Constantin <mordredur@googlemail.com>
2019-12-02 21:27:59 +0100

    adds setup script

---
+++ setupNewEnv.sh
@@ -0,0 +1,15 @@
+#This script sets up dotfiles on a new environment
+git clone --bare https://github.com/phosfox/dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
